5ceec4b264ec163b0a7b1f45ace051864cad3253
Franco Pellegrini <frapell@gmail.com>
2014-10-26 22:20:30 +0000

    Include less by default with the development

--- app/templates/dev/_dev.js
+++ app/templates/dev/_dev.js
@@ -11,6 +11,25 @@
       require(['mockup-bundles-<%= _.slugify(packageName) %>']);
     };
 
+    var splitted_location = document.URL.split('/');
+    var current_folder = splitted_location.splice(2, splitted_location.length-4).join('/');
+
+    var style1 = document.createElement('style');
+    style1.setAttribute('type', 'text/less');
+    style1.innerHTML = '\n\
+                        @import (less) "../less/<%= _.slugify(packageName) %>.less";\n\
+                        @isBrowser: true;\n\
+                        @pathPrefix: \'../less/\';\n\
+                        @bowerPath: \''+current_folder+'/bower_components\';\n\
+                        @mockupPath: \'../bower_components/plone-mockup/patterns"\';\n\
+                        @mockuplessPath: \'"less/"\';';
+    document.getElementsByTagName("head")[0].appendChild(style1);
+
+    var script_less = document.createElement('script');
+    script_less.setAttribute('type', 'text/javascript');
+    script_less.setAttribute('src', '../node_modules/less/dist/less-1.7.5.js');
+    document.getElementsByTagName("head")[0].appendChild(script_less);
+
     var script_require = document.createElement('script');
     script_require.setAttribute('type', 'text/javascript');
     script_require.setAttribute('src', '../node_modules/requirejs/require.js');
@@ -19,17 +38,6 @@
     };
     document.getElementsByTagName("head")[0].appendChild(script_require);
 
-    /* Uncoment the following if you need to use 'less'
-    var style1 = document.createElement('style');
-    style1.setAttribute('type', 'text/less');
-    style1.innerHTML = '@import (less) "../less/bundle.less"; @isBrowser: true; @pathPrefix: \'../less/\';';
-    document.getElementsByTagName("head")[0].appendChild(style1);
-
-    var script_less = document.createElement('script');
-    script_less.setAttribute('type', 'text/javascript');
-    script_less.setAttribute('src', '../node_modules/grunt-contrib-less/node_modules/less/dist/less-1.4.1.js');
-    document.getElementsByTagName("head")[0].appendChild(script_less);
-    */
   });
 
 }(jQuery));
