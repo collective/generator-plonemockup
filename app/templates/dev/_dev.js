c1ef5e7b73df10b8ec07d33962369fa813855df6
Franco Pellegrini <frapell@gmail.com>
2014-11-02 11:24:42 +0000

    Missing a curly brace

--- app/templates/dev/_dev.js
+++ app/templates/dev/_dev.js
@@ -3,14 +3,6 @@
 
   $(document).ready(function() {
 
-    var script_config = document.createElement('script');
-    script_config.setAttribute('type', 'text/javascript');
-    script_config.setAttribute('src', '../config.js');
-    script_config.onload = function() {
-      requirejs.config({ baseUrl: '../' });
-      require(['mockup-bundles-<%= _.slugify(packageName) %>']);
-    };
-
     var splitted_location = document.URL.split('/');
     var current_folder = splitted_location.splice(2, splitted_location.length-4).join('/');
 
@@ -29,14 +21,22 @@
     script_less.setAttribute('type', 'text/javascript');
     script_less.setAttribute('src', '../node_modules/less/dist/less-1.7.5.js');
     document.getElementsByTagName("head")[0].appendChild(script_less);
-
-    var script_require = document.createElement('script');
-    script_require.setAttribute('type', 'text/javascript');
-    script_require.setAttribute('src', '../node_modules/requirejs/require.js');
-    script_require.onload = function() {
-      document.getElementsByTagName("head")[0].appendChild(script_config);
+    script_less.onload = function() {
+        var script_require = document.createElement('script');
+        script_require.setAttribute('type', 'text/javascript');
+        script_require.setAttribute('src', '../node_modules/requirejs/require.js');
+        document.getElementsByTagName("head")[0].appendChild(script_require);
+        script_require.onload = function() {
+            var script_config = document.createElement('script');
+            script_config.setAttribute('type', 'text/javascript');
+            script_config.setAttribute('src', '../config.js');
+            document.getElementsByTagName("head")[0].appendChild(script_config);
+            script_config.onload = function() {
+                requirejs.config({ baseUrl: '../' });
+                require(['mockup-bundles-<%= _.slugify(packageName) %>']);
+            };
+        };
     };
-    document.getElementsByTagName("head")[0].appendChild(script_require);
 
   });
 
